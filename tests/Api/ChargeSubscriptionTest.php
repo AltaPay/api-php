84e29d17c954a7dac932a1796d3d8bead6ca511c
Shahbaz <shahbaz.mehar@outlook.com>
2023-04-18 12:12:44 +0500

    Update ChargeSubscriptionTest with POST method

--- tests/Api/ChargeSubscriptionTest.php
+++ tests/Api/ChargeSubscriptionTest.php
@@ -52,8 +52,8 @@ class ChargeSubscriptionTest extends AbstractApiTest
         $api->call();
         $request = $api->getRawRequest();
 
-        $this->assertSame($this->getExceptedUri('chargeSubscription/'), $request->getUri()->getPath());
-        parse_str($request->getUri()->getQuery(), $parts);
+        $this->assertSame($this->getExceptedUri('chargeSubscription'), $request->getUri()->getPath());
+        parse_str($request->getBody()->getContents(), $parts);
         $this->assertSame('456', $parts['transaction_id']);
 
         $api = $this->getChargeSubscription();
@@ -62,7 +62,7 @@ class ChargeSubscriptionTest extends AbstractApiTest
         $api->setReconciliationIdentifier('my identifier');
         $api->call();
         $request = $api->getRawRequest();
-        parse_str($request->getUri()->getQuery(), $parts);
+        parse_str($request->getBody()->getContents(), $parts);
         $this->assertSame('helloworld', $parts['transaction_id']);
         $this->assertSame('200.5', $parts['amount']);
         $this->assertSame('my identifier', $parts['reconciliation_identifier']);
@@ -71,7 +71,7 @@ class ChargeSubscriptionTest extends AbstractApiTest
         $api->setTransaction('my trans id has spaces');
         $api->call();
         $request = $api->getRawRequest();
-        parse_str($request->getUri()->getQuery(), $parts);
+        parse_str($request->getBody()->getContents(), $parts);
         $this->assertSame('my trans id has spaces', $parts['transaction_id']);
     }
 
